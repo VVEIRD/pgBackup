a0b2108b5ebde21820f99934908e4c36b0a94844
VVEIRD <vveird.oss@gmail.com>
2020-01-13 01:09:12 +0100

    Added script to detect preinstalled clusters

---
+++ u00/scripts/backup/tools/register_cluster.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+#=================================================#
+# Register existing Cluster to /etc/pgcluster     #
+#                                                 #
+# Author: Roland von Werden                       #
+# Release: 0.1                                    #
+# License: MIT                                    #
+#=================================================#
+
+import os, getpass, sys, subprocess, glob, configparser
+from datetime import datetime
+
+if getpass.getuser() != "root":
+    print("This script has to be executed as root, try su - root -c 'python3 %s'" % (os.path.realpath(__file__)))
+    sys.exit(1)
+
+pgConfigs = []
+pgcluster = ''
+with open('/etc/pgcluster', 'r') as content_file:
+    for pgcString in content_file:
+        if not pgcString.startswith('#'):
+            pgcluster += pgcString + '\r\n'
+
+# Find possible postgresql configurations
+for root, dirs, files in os.walk('/'):
+    # Ignore hidden files
+    if '.' in root:
+        continue
+    for file in files:
+        if file == 'postgresql.conf' and root not in pgcluster:
+            print('PostgreSQL configuration found: %s' % os.path.join(root, file))
+            pgConfigs.append(os.path.join(root, file))
+
+print('')
+
+for cfgFile in pgConfigs:
+    print('Add "%s" to "/etc/pgcluster"? (Y/n)' % cfgFile)
+    ch = input()
+    if ch.strip().lower() == 'n':
+        continue
+    config_string = ''
+    with open(cfgFile, 'r') as f:
+       config_string = '[DEFAULT]\n' + f.read()
+    pgConfig = configparser.ConfigParser()
+    pgConfig.read_string(config_string)
+    pgData = os.sep.join(cfgFile.split(os.sep)[0:-1])
+    print('Using PGDATA: %s' % pgData)
+    port = 5432
+    if pgConfig.has_option('DEFAULT', 'port'):
+        print('Port found: %s' % pgConfig['DEFAULT']['port'])
+        port = int(pgConfig['DEFAULT']['port'])
+    else:
+        print('Using default port 5432')
+    # Try reading binaries from postmaster.opts
+    pgBin = None
+    postmasterOpts = os.sep.join([pgData, 'postmaster.opts'])
+    if os.path.exists(postmasterOpts):
+        postmaster_opts_data = ''
+        with open(postmasterOpts, 'r') as f:
+           postmaster_opts_data = f.read()
+        pgBin = os.sep.join(postmaster_opts_data.split(' ')[0].split(os.sep)[0:-2])
+    if pgBin != '':
+        print('Found PG Binaries: %s' % pgBin)
+    pgVersionFile = os.sep.join([pgData, 'PG_VERSION'])
+    pgVersion= None
+    if os.path.exists(pgVersionFile):
+        pgVersion = ''
+        with open(pgVersionFile, 'r') as f:
+           pgVersion = f.read().strip()
+    pgBackup = '1'
+    print('Activate backup? (Y/n)')
+    ch = input()
+    if ch.strip().lower() == 'n':
+        pgBackup = '0'
+    print('')
+    print('')
+    print('Please check if the following configuration is correct:')
+    print('')
+    print('PostgreSQL Binaries: %s' % pgBin)
+    print('            Cluster: %s' % pgData)
+    print('               Port: %s' % port)
+    print('            Version: %s' % pgVersion)
+    print('      Active Backup: %s' % pgBackup)
+    print('')
+    if pgBin == None or pgVersion == None:
+        print('Not all configuration could be detected, please add the cluster manually!')
+        continue
+    print('Add cluster to /etc/pgcluster? (y/N)')
+    ch = input()
+    if ch.strip().lower() == 'y':
+        with open('/etc/pgcluster', 'a') as file:
+            file.write('%s:%s:%s:%s:1:%s\n' % (pgData, str(port), pgBin, str(pgVersion), str(pgBackup)))
